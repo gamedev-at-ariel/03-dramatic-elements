a37cf7b134202cf4bf73daaa00bde9fb79c8e82f
Erel Segal-Halevi <erelsgl@gmail.com>
2020-05-07 15:47:36 +0300

    last week changes

---
+++ Assets/Scripts/3-collisions/HorizontalTeleporter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/**
+ * This component teleports any object that triggers it to another object.
+ */
+public class HorizontalTeleporter: MonoBehaviour {
+    [SerializeField] Transform teleportTarget;
+    
+    private void OnTriggerEnter2D(Collider2D other) {
+        other.transform.position = new Vector3(teleportTarget.position.x, other.transform.position.y, other.transform.position.z);
+    }
+}
